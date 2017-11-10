bf55c9e9ab0bc18de985ff47d07ff3694b141782
Robin Goh <gohwangfoo@gmail.com>
2017-11-09 16:56:47 -0800

    passed test case for searchWithDifferentCriteriaAfter1stSearch

--- app/src/androidTest/java/edu/sjsu/team408/parkhere/SearchResultActivityTest.java
+++ app/src/androidTest/java/edu/sjsu/team408/parkhere/SearchResultActivityTest.java
@@ -10,6 +10,8 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.intent.Intents.intended;
@@ -23,6 +25,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 public class SearchResultActivityTest {
     private String dateStringToBeEntered;
     private String locationStringToBeTyped;
+    private String altLocationStringToBeTyped;
 
     @Rule
     public ActivityTestRule<MainActivity> activityActivityTestRule = new
@@ -31,6 +34,7 @@ public class SearchResultActivityTest {
     public void initStringInput() {
         dateStringToBeEntered = "11-10-2017";
         locationStringToBeTyped = "64 S 4th St, San Jose";
+        altLocationStringToBeTyped = "447 Great Mall Dr, Milpitas, CA 95035";
     }
 
     @Test
@@ -58,11 +62,29 @@ public class SearchResultActivityTest {
         onView(withId(R.id.searchBtn))
                 .perform(click());
         slowDown2Secs();
-
     }
     @Test
-    public void searchWithDifferentCriteriaAfter1stSearch() {}
+    public void searchWithDifferentCriteriaAfter1stSearch() {
+        onView(withId(R.id.locationSearchTerm))
+                .perform(typeText(locationStringToBeTyped));
+        closeSoftKeyboard();
+        HomeFragment.setDate(dateStringToBeEntered);
+        onView(withId(R.id.searchBtn))
+                .perform(click());
+        slowDown2Secs();
 
+        pressBack();
+
+        onView(withId(R.id.locationSearchTerm))
+                .perform(clearText(), typeText(altLocationStringToBeTyped));
+        slowDown2Secs();
+        closeSoftKeyboard();
+        onView(withId(R.id.searchBtn))
+                .perform(click());
+        slowDown2Secs();
+    }
+
+    // A function to delay the automated test in order to see it in action.
     private void slowDown2Secs() {
         try {
             Thread.sleep(2000);
