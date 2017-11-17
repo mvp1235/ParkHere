002a303fb745e6bf310a3e409f0a960d2139fc9c
Duoc Nguyen <duocnguyen1996@gmail.com>
2017-11-17 03:49:28 -0800

    seeker now able to set their own booking range in terms of date and time.

--- app/src/main/java/edu/sjsu/team408/parkhere/DetailParkingActivity.java
+++ app/src/main/java/edu/sjsu/team408/parkhere/DetailParkingActivity.java
@@ -1,17 +1,24 @@
 package edu.sjsu.team408.parkhere;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -25,9 +32,17 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
 import java.io.IOException;
+import java.util.Calendar;
 
 public class DetailParkingActivity extends AppCompatActivity {
+
+    private final static int FROM_DATE = 0;
+    private final static int TO_DATE = 1;
+    private final static int FROM_TIME = 2;
+    private final static int TO_TIME = 3;
 
     private TextView addressTV, ownerTV, specialInstructionTV, dateTV, priceTV;
     private ImageView parkingPhoto;
@@ -40,6 +55,9 @@ public class DetailParkingActivity extends AppCompatActivity {
     private ParkingSpace chosenParking;
     private static String parkingPhotoString = "https://d30y9cdsu7xlg0.cloudfront.net/png/47205-200.png";
     private Uri parkingURI = null;
+    private TextView reserveToTime, reserveFromDate, reserveToDate, reserveFromTime;
+    private Calendar calendar;
+    private int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +76,48 @@ public class DetailParkingActivity extends AppCompatActivity {
         priceTV = (TextView) findViewById(R.id.detailParkingPrice);
         parkingPhoto = (ImageView) findViewById(R.id.detailParkingPhoto);
         reserveBtn = (Button) findViewById(R.id.reserveBtn);
+
+        //seeker set reservation from date to date, from time to time.
+        reserveFromDate = (TextView) findViewById(R.id.reserveFromDateTV);
+        reserveToDate = (TextView) findViewById(R.id.reserveToDateTV);
+        reserveFromTime = (TextView) findViewById(R.id.reserveFromTimeTV);
+        reserveToTime = (TextView) findViewById(R.id.reserveToTimeTV);
+
+        //Get the current day, month, and year
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        //Set on click listeners for the date and time pickers
+        reserveFromDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(FROM_DATE);
+            }
+        });
+        reserveToDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(TO_DATE);
+            }
+        });
+        reserveFromTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(FROM_TIME);
+            }
+        });
+        reserveToTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(TO_TIME);
+            }
+        });
+
+
+
+
 
         Intent intent = getIntent();
 
@@ -177,4 +237,124 @@ public class DetailParkingActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+
+        //Depending on which element ID was called, the appropriate date/time picker will be displayed to the user
+        switch (id) {
+            case FROM_DATE:
+                return new DatePickerDialog(this, fromDateListener, year, month, day);
+            case TO_DATE:
+                return new DatePickerDialog(this, toDateListener, year, month, day);
+            case FROM_TIME:
+                return new TimePickerDialog(this, fromTimeListener, 12, 0,false);
+            case TO_TIME:
+                return new TimePickerDialog(this, toTimeListener, 12, 0,false);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener fromDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    setReserveFromDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private DatePickerDialog.OnDateSetListener toDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    setReserveToDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private TimePickerDialog.OnTimeSetListener fromTimeListener = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view,int hourofday,int min){
+            setReserveFromTime(hourofday, min);
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener toTimeListener = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view,int hourofday,int min){
+            setReserveToTime(hourofday, min);
+        }
+    };
+
+    public void setReserveFromTime(int hour, int minute) {
+        String hourString, minuteString;
+        String ampm = "AM";
+
+        if (hour > 12) {
+            hour = hour % 12;
+            ampm = "PM";
+        } else if (hour == 0) {
+            hour = 12;
+        } else if (hour == 12) {
+            ampm = "PM";
+        }
+
+        hourString = Integer.toString(hour);
+        minuteString = (minute < 10) ? "0" + Integer.toString(minute) : Integer.toString(minute);
+        String completeTime = hourString + ":" + minuteString + " " + ampm;
+
+        reserveFromTime.setText("From Time: " + completeTime);
+    }
+
+    public void setReserveToTime(int hour, int minute) {
+        String hourString, minuteString;
+        String ampm = "AM";
+
+        if (hour > 12) {
+            hour = hour % 12;
+            ampm = "PM";
+        } else if (hour == 0) {
+            hour = 12;
+        } else if (hour == 12) {
+            ampm = "PM";
+        }
+
+        hourString = Integer.toString(hour);
+        minuteString = (minute < 10) ? "0" + Integer.toString(minute) : Integer.toString(minute);
+        String completeTime = hourString + ":" + minuteString + " " + ampm;
+
+        reserveToTime.setText("To Time: " + completeTime);
+    }
+
+    public void setReserveFromDate(int year, int month, int day) {
+        String yearString, monthString, dayString;
+        yearString = Integer.toString(year);
+        monthString = (month < 10) ? "0" + Integer.toString(month) : Integer.toString(month);
+        dayString = (day < 10) ? "0" + Integer.toString(day) : Integer.toString(day);
+
+        String completeDate = monthString + "-" + dayString + "-" + yearString;
+        reserveFromDate.setText("From Date: " +completeDate);
+    }
+
+    public void setReserveToDate(int year, int month, int day) {
+        String yearString, monthString, dayString;
+        yearString = Integer.toString(year);
+        monthString = (month < 10) ? "0" + Integer.toString(month) : Integer.toString(month);
+        dayString = (day < 10) ? "0" + Integer.toString(day) : Integer.toString(day);
+
+        String completeDate = monthString + "-" + dayString + "-" + yearString;
+        reserveToDate.setText("To Date: " + completeDate);
+    }
+
+
 }
