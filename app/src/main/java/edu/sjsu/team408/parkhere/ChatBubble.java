85c6631b8d8c30cabba7e606bec12bc5a14b93af
robingoh <gohwangfoo@gmail.com>
2018-03-22 14:13:21 -0700

    added javadoc for ChatBubble.java

--- app/src/main/java/edu/sjsu/team408/parkhere/ChatBubble.java
+++ app/src/main/java/edu/sjsu/team408/parkhere/ChatBubble.java
@@ -8,15 +8,28 @@ public class ChatBubble {
     private Message message;
     private boolean isMyMessage;
 
+    /**
+     * Construct a ChatBubble object.
+     * @param message a message object
+     * @param isMyMessage a boolean value of whether the message belongs to this chat owner.
+     */
     public ChatBubble(Message message, boolean isMyMessage) {
         this.message = message;
         this.isMyMessage = isMyMessage;
     }
 
+    /**
+     * Get the message object.
+     * @return message
+     */
     public Message getMessage() {
         return message;
     }
 
+    /**
+     * Get the boolean value isMyMessage.
+     * @return isMyMessage
+     */
     public boolean getIsMyMessage() {
         return isMyMessage;
     }
