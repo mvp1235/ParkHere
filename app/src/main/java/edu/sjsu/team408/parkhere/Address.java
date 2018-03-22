e6d5ff38bf56c28b2d8d1d955c400134f90a2b91
Huy Nguyen <h.nguyen1235@gmail.com>
2018-03-22 14:23:46 -0700

    BookingHistory, BookingReview, Address documentation

--- app/src/main/java/edu/sjsu/team408/parkhere/Address.java
+++ app/src/main/java/edu/sjsu/team408/parkhere/Address.java
@@ -18,9 +18,8 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- * 
+ * Model for Address
  */
-
 public class Address implements Parcelable {
     private static final double MILES_TO_METER = 1609.344;
 
@@ -31,9 +30,21 @@ public class Address implements Parcelable {
     private double latitude;
     private double longitude;
 
+    /**
+     * Default constructor for Address
+     */
     public Address() {
     }
 
+    /**
+     * Constructor for Address
+     * @param streetAddress street number and name of the address
+     * @param city city of the address
+     * @param state state of the address
+     * @param zipCode zip code of the address
+     * @param latitude latitude of the address
+     * @param longitude longitude of the address
+     */
     public Address(String streetAddress, String city, String state, String zipCode,
                    double latitude, double longitude) {
         if(streetAddress.isEmpty() || city.isEmpty() || state.isEmpty() || zipCode.isEmpty()) {
@@ -47,6 +58,10 @@ public class Address implements Parcelable {
         this.longitude = longitude;
     }
 
+    /**
+     * Constructor for Address
+     * @param in the Parcelable object which contains all necessary information for Address
+     */
     protected Address(Parcel in) {
         streetAddress = in.readString();
         city = in.readString();
@@ -56,7 +71,11 @@ public class Address implements Parcelable {
         longitude = in.readDouble();
     }
 
-
+    /**
+     * Constructor for Address
+     * @param address full address
+     * @param point latitude and longitude of the address
+     */
     public Address(String address, LatLng point) {
         formatAddress(address, point);
     }
@@ -73,6 +92,10 @@ public class Address implements Parcelable {
         }
     };
 
+    /**
+     * Gets the full string representation of the address
+     * @return full address, e.g. street number and name, city, state, zip code
+     */
     public String toString(){
         if(streetAddress == null || city == null || state == null || zipCode == null) {
             return "";
@@ -81,50 +104,102 @@ public class Address implements Parcelable {
         return address;
     }
 
+    /**
+     * Sets the street address
+     * @param streetAddress the street address to be set
+     */
     public void setStreetAddress(String streetAddress) {
         this.streetAddress = streetAddress;
     }
 
+    /**
+     * Sets the city
+     * @param city the city to be set
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * Sets the zip code
+     * @param zipCode the zip code to be set
+     */
     public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
 
+    /**
+     * Sets the state
+     * @param state the state to be set
+     */
     public void setState(String state) { this.state = state;}
 
+    /**
+     * Sets the latitude
+     * @param latitude the latitude to be set
+     */
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
+    /**
+     * Sets the longitude
+     * @param longitude the longitude to be set
+     */
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
 
+    /**
+     * Gets the full Latitude/Longitude coordinate
+     * @return a string representation of the coordinate, (lat, lng)
+     */
     public String getFullLatLngString() {
         return latitude + ", " + longitude;
     }
 
+    /**
+     * Gets the state
+     * @return state
+     */
     public String getState() { return state;}
 
+    /**
+     * Gets the street address
+     * @return streetAddress
+     */
     public String getStreetAddress() {
         return streetAddress;
     }
 
+    /**
+     * Gets the city
+     * @return city
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Gets the zip code
+     * @return zipCode
+     */
     public String getZipCode() {
         return zipCode;
     }
 
+    /**
+     * Gets the latitude
+     * @return latitude
+     */
     public double getLatitude() {
         return latitude;
     }
 
+    /**
+     * Gets the longitude
+     * @return longitude
+     */
     public double getLongitude() {
         return longitude;
     }
@@ -161,12 +236,17 @@ public class Address implements Parcelable {
         dest.writeDouble(longitude);
     }
 
-    //The address should follow this format:
-    // One Washington Square, San Jose, CA 95112
-    // Token 1 = One Washington Square
-    // Token 2 = San Jose
-    // Token 3 = CA 95112
-    // Separate Token 3 to get state code and zip code
+    /**
+     * Format the address to the proper format
+     * @param address the full address
+     * @param point full coordinate of the address
+     * The address should follow this format:
+     * One Washington Square, San Jose, CA 95112
+     * Token 1 = One Washington Square
+     * Token 2 = San Jose
+     * Token 3 = CA 95112
+     * Separate Token 3 to get state code and zip code
+     */
     public void formatAddress(String address, LatLng point){
         if(address.isEmpty() || point == null) {
             return;
@@ -189,7 +269,11 @@ public class Address implements Parcelable {
     }
 
 
-
+    /**
+     * Calculates the distance between the current location to the specified location
+     * @param thatLocation the other location to be used
+     * @return the distace from the current location to the specified location
+     */
     public double getDistanceBetweenThisAnd(Location thatLocation) {
         Location thisLocation = new Location("");
 
